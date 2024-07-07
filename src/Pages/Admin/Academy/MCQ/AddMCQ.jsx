ba6f10848c78b806ebcff34ecda22d92dbb1d825
nuaimhasan <nuaim.emanager@gmail.com>
2024-07-07 19:22:08 +0600

    add mcq start

--- src/Pages/Admin/Academy/MCQ/AddMCQ.jsx
+++ src/Pages/Admin/Academy/MCQ/AddMCQ.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+import JoditEditor from "jodit-react";
+import { useGetAcademyCategoriesQuery } from "../../../../Redux/api/academy/categoryApi";
+import { useGetAcademyClassesQuery } from "../../../../Redux/api/academy/classApi";
+import { useGetAcademySubjectsQuery } from "../../../../Redux/api/academy/subjectApi";
+
 export default function AddMCQ() {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+
+  //------------------------Category
+  const { data: category } = useGetAcademyCategoriesQuery();
+  const categories = category?.data;
+  const [selectedCategory, setSelectedCategory] = useState("");
+  useEffect(() => {
+    setSelectedCategory(category?.data[0]?._id);
+  }, [category?.data]);
+
+  //------------------------Classes
+  const { data: cls } = useGetAcademyClassesQuery(selectedCategory);
+  const classes = cls?.data;
+  const [selectedClass, setSelectedClass] = useState("");
+  useEffect(() => {
+    setSelectedClass(cls?.data[0]?._id);
+  }, [cls?.data]);
+
+  //---------------------Subject
+  const { data: subject } = useGetAcademySubjectsQuery(selectedClass);
+  const subjects = subject?.data;
+  const [selectedSubject, setSelectedSubject] = useState("");
+  useEffect(() => {
+    setSelectedSubject(subject?.data[0]?._id);
+  }, [subject?.data]);
+
+  console.log(selectedSubject, content);
+
   return (
     <section>
       <div className="bg-base-100 rounded shadow">
@@ -7,33 +42,65 @@ export default function AddMCQ() {
         <form className="p-4">
           <div className="grid sm:grid-cols-3 gap-4">
             <div>
-              <p className="mb-1">Content</p>
-              <input type="text" name="title" required />
-            </div>
-            <div>
-              <p className="mb-1">Chapter Name</p>
-              <select name="chapter" required>
-                <option value=""></option>
+              <p className="mb-1">Category Name</p>
+              <select
+                name="category"
+                required
+                onChange={(e) => setSelectedCategory(e.target.value)}
+              >
+                {categories?.map((category) => (
+                  <option key={category?._id} value={category?._id}>
+                    {category?.name}
+                  </option>
+                ))}
               </select>
             </div>
-            <div>
-              <p className="mb-1">Subject Name</p>
-              <select name="subject" required>
-                <option value=""></option>
-              </select>
-            </div>
+
             <div>
               <p className="mb-1">Class Name</p>
-              <select name="class" required>
-                <option value=""></option>
+              <select
+                name="class"
+                required
+                onChange={(e) => setSelectedClass(e.target.value)}
+              >
+                {classes?.map((clas) => (
+                  <option key={clas?._id} value={clas?._id}>
+                    {clas?.name}
+                  </option>
+                ))}
               </select>
             </div>
+
             <div>
-              <p className="mb-1">Category Name</p>
-              <select name="category" required>
-                <option value=""></option>
+              <p className="mb-1">Subject Name</p>
+              <select
+                name="subject"
+                required
+                onChange={(e) => setSelectedSubject(e.target.value)}
+              >
+                {subjects?.map((subject) => (
+                  <option key={subject?._id} value={subject?._id}>
+                    {subject?.name}
+                  </option>
+                ))}
               </select>
             </div>
+          </div>
+
+          <div className="mt-4 grid sm:grid-cols-3 gap-4">
+            <div>
+              <p className="mb-1">Question</p>
+              <input type="text" name="question" required />
+            </div>
+          </div>
+
+          <div className=" mt-4 content">
+            <p className="mb-1">ব্যাখ্যা</p>
+            <JoditEditor
+              ref={editor}
+              value=""
+              onBlur={(text) => setContent(text)}
+            />
           </div>
 
           <div className="mt-2">
